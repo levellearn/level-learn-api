49432402bf4607bcd2710d55010ce5066eaed76a
Gabriel Faria <gggfaria@outlook.com>
2020-10-10 01:02:33 -0300

    implementando fixture e automocker turma test

---
+++ LevelLearn.XUnitTest/Fixtures/TurmaTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using LevelLearn.Service.Services.Institucional;
+using Moq.AutoMock;
+using System;
+using Xunit;
+
+namespace LevelLearn.XUnitTest.Fixtures
+{
+    [CollectionDefinition(nameof(TurmaAutoMockerCollection))]
+    public class TurmaAutoMockerCollection: ICollectionFixture<TurmaTestFixture> { }
+
+    public class TurmaTestFixture : IDisposable
+    {
+        public AutoMocker AutoMocker { get; private set; }
+
+        public TurmaTestFixture()
+        {
+            AutoMocker = new AutoMocker();
+        }
+
+        public TurmaService ObterTurmaService()
+        {
+            return AutoMocker.CreateInstance<TurmaService>();
+        }
+
+
+        public void Dispose()
+        {
+
+        }
+    }
+
+}
--- LevelLearn.XUnitTest/ServiceTest/Institucional/TurmaServiceTest.cs
+++ LevelLearn.XUnitTest/ServiceTest/Institucional/TurmaServiceTest.cs
@@ -1,18 +1,18 @@
 ﻿using LevelLearn.Domain.Entities.Institucional;
 using LevelLearn.Domain.Repositories.Institucional;
 using LevelLearn.Domain.UnityOfWorks;
-using LevelLearn.Resource;
 using LevelLearn.Service.Response;
 using LevelLearn.Service.Services.Institucional;
 using LevelLearn.XUnitTest.DomainTest;
+using LevelLearn.XUnitTest.Fixtures;
 using Moq;
-using System;
 using System.Net;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace LevelLearn.XUnitTest.ServiceTest.Institucional
 {
+    [Collection(nameof(TurmaAutoMockerCollection))]
     public class TurmaServiceTest
     {
         //AAA Arrange, Act, Assert
@@ -21,9 +21,12 @@ namespace LevelLearn.XUnitTest.ServiceTest.Institucional
         //MetodoTestado_EstadoEmTeste_ComportamentoEsperado
         //ObjetoEmTeste_MetodoComportamentoEmTeste_ComportamentoEsperado
 
-        public TurmaServiceTest()
+        private readonly TurmaTestFixture _turmaFixture;
+        private readonly TurmaService _turmaService;
+        public TurmaServiceTest(TurmaTestFixture turmaFixture)
         {
-
+            _turmaFixture = turmaFixture;
+            _turmaService = turmaFixture.ObterTurmaService();
         }
 
         [Trait("Categoria", "Turmas")]
@@ -32,17 +35,14 @@ namespace LevelLearn.XUnitTest.ServiceTest.Institucional
         {
             var turma = FakerTest.CriarTurmaPadrao();
             var turmaId = turma.Id;
-            var sharedResource = new Mock<ISharedResource>();
-            var uow = new Mock<IUnitOfWork>();
-            var turmaRepository = new Mock<ITurmaRepository>();
-            uow.Setup(uow => uow.Turmas).Returns(turmaRepository.Object);
-            turmaRepository.Setup(p => p.GetAsync(turmaId, true)).Returns(Task.FromResult(turma));
+            
+            var turmaRepositoryMock = new Mock<ITurmaRepository>();
+            _turmaFixture.AutoMocker.GetMock<IUnitOfWork>().Setup(uow => uow.Turmas).Returns(turmaRepositoryMock.Object);
+            turmaRepositoryMock.Setup(p => p.GetAsync(turmaId, true)).Returns(Task.FromResult(turma));
 
-            var turmaService = new TurmaService(uow.Object, sharedResource.Object);
+            ResultadoService<Turma> resultadoService = _turmaService.ObterTurma(turmaId).Result;
 
-            ResultadoService<Turma> resultadoService = turmaService.ObterTurma(turmaId).Result;
-
-            uow.Verify(r => r.Turmas.GetAsync(turmaId, true), Times.Once);
+            _turmaFixture.AutoMocker.GetMock<IUnitOfWork>().Verify(r => r.Turmas.GetAsync(turmaId, true), Times.Once);
             Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)resultadoService.StatusCode);
         }
 
