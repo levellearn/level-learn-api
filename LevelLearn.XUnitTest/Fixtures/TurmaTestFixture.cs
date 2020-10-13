819e40107b8a4dc8f3e606b9cfd721338c7193e1
Gabriel Faria <gggfaria@outlook.com>
2020-10-12 23:01:30 -0300

    add test cadastrar turma - turma service

--- LevelLearn.XUnitTest/Fixtures/TurmaTestFixture.cs
+++ LevelLearn.XUnitTest/Fixtures/TurmaTestFixture.cs
@@ -1,6 +1,10 @@
-﻿using LevelLearn.Service.Services.Institucional;
+﻿using LevelLearn.Domain.Repositories.Institucional;
+using LevelLearn.Domain.UnityOfWorks;
+using LevelLearn.Service.Services.Institucional;
+using Moq;
 using Moq.AutoMock;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace LevelLearn.XUnitTest.Fixtures
@@ -19,7 +23,24 @@ namespace LevelLearn.XUnitTest.Fixtures
 
         public TurmaService ObterTurmaService()
         {
-            return AutoMocker.CreateInstance<TurmaService>();
+            var turmaService = AutoMocker.CreateInstance<TurmaService>();
+            AutoMocker.GetMock<IUnitOfWork>().Setup(uow => uow.CommitAsync()).Returns(Task.FromResult(true));
+            AutoMocker.GetMock<IUnitOfWork>().Setup(uow => uow.Commit()).Returns(true);
+            return turmaService;
+        }
+
+        public Mock<ITurmaRepository> SetupTurmaRepositoryUoW()
+        {
+            var turmaRepositoryMock = new Mock<ITurmaRepository>();
+            AutoMocker.GetMock<IUnitOfWork>().Setup(uow => uow.Turmas).Returns(turmaRepositoryMock.Object);
+            return turmaRepositoryMock;
+        }
+
+        public Mock<ICursoRepository> SetupCursoRepositoryUoW()
+        {
+            var cursoRepository = new Mock<ICursoRepository>();
+            AutoMocker.GetMock<IUnitOfWork>().Setup(uow => uow.Cursos).Returns(cursoRepository.Object);
+            return cursoRepository;
         }
 
 
--- LevelLearn.XUnitTest/ServiceTest/Institucional/TurmaServiceTest.cs
+++ LevelLearn.XUnitTest/ServiceTest/Institucional/TurmaServiceTest.cs
@@ -8,8 +8,10 @@ using LevelLearn.Service.Services.Institucional;
 using LevelLearn.XUnitTest.DomainTest;
 using LevelLearn.XUnitTest.Fixtures;
 using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net;
 using System.Threading.Tasks;
 using Xunit;
@@ -42,9 +44,8 @@ namespace LevelLearn.XUnitTest.ServiceTest.Institucional
         {
             var turma = FakerTest.CriarTurmaPadrao();
             var turmaId = turma.Id;
-            
-            var turmaRepositoryMock = new Mock<ITurmaRepository>();
-            _turmaFixture.AutoMocker.GetMock<IUnitOfWork>().Setup(uow => uow.Turmas).Returns(turmaRepositoryMock.Object);
+
+            var turmaRepositoryMock = _turmaFixture.SetupTurmaRepositoryUoW();
             turmaRepositoryMock.Setup(p => p.GetAsync(turmaId, true)).Returns(Task.FromResult(turma));
 
             ResultadoService<Turma> resultadoService = _turmaService.ObterTurma(turmaId).Result;
@@ -63,8 +64,7 @@ namespace LevelLearn.XUnitTest.ServiceTest.Institucional
             var professorId = turmasFake.First().ProfessorId;
             var filtroPag = new FiltroPaginacao();
 
-            var turmaRepositoryMock = new Mock<ITurmaRepository>();
-            _turmaFixture.AutoMocker.GetMock<IUnitOfWork>().Setup(uow => uow.Turmas).Returns(turmaRepositoryMock.Object);
+            var turmaRepositoryMock = _turmaFixture.SetupTurmaRepositoryUoW();
             turmaRepositoryMock.Setup(p => p.TurmasProfessorPorCurso(cursoId, professorId, filtroPag)).Returns(Task.FromResult(turmasFake));
 
             ResultadoService<IEnumerable<Turma>> resultadoService = _turmaService.TurmasProfessorPorCurso(cursoId, professorId, filtroPag).Result;
@@ -72,7 +72,35 @@ namespace LevelLearn.XUnitTest.ServiceTest.Institucional
             _turmaFixture.AutoMocker.GetMock<IUnitOfWork>().Verify(r => r.Turmas.TurmasProfessorPorCurso(cursoId, professorId, filtroPag), Times.Once);
             IEnumerable<ValidationFailure> errosValidacao = turmasFake.SelectMany(p => p.ResultadoValidacao.Errors);
             _testOutputHelper.WriteLine($"Erros encontrados na validação de turma: {errosValidacao.Count()} {string.Join(", ", errosValidacao)}");
+            Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)resultadoService.StatusCode);
         }
 
+        [Trait("Categoria", "Turmas")]
+        [Fact]
+        public void TurmaService_CadastrarTurma_DeveCriar()
+        {
+            Turma turma = FakerTest.CriarTurmaPadrao();
+            var cursoRepositoryMock = _turmaFixture.SetupCursoRepositoryUoW();
+            cursoRepositoryMock.Setup(p => p.EntityExists(i => i.Id == turma.CursoId)).Returns(Task.FromResult(true));
+            cursoRepositoryMock.Setup(p => p.ProfessorDoCurso(turma.CursoId, turma.ProfessorId)).Returns(Task.FromResult(true));
+
+            var turmaRepositoryMock = _turmaFixture.SetupTurmaRepositoryUoW();
+            turmaRepositoryMock.Setup(TurmaExisteNoCurso(turma)).Returns(Task.FromResult(false));
+
+
+            ResultadoService<Turma> resultadoService = _turmaService.CadastrarTurma(turma, turma.ProfessorId).Result;
+
+            _turmaFixture.AutoMocker.GetMock<IUnitOfWork>().Verify(r => r.Turmas.AddAsync(turma), Times.Once);
+            _testOutputHelper.WriteLine($"Erros encontrados na validação de turma: {turma.ResultadoValidacao.Errors.Count()} {string.Join(", ", turma.ResultadoValidacao.Errors)}");
+            Assert.Equal(HttpStatusCode.Created, (HttpStatusCode)resultadoService.StatusCode);
+        }
+
+        private static Expression<Func<ITurmaRepository, Task<bool>>> TurmaExisteNoCurso(Turma turma)
+        {
+            return p => p.EntityExists(t =>
+                            t.NomePesquisa == turma.NomePesquisa &&
+                            t.Id != turma.Id &&
+                            t.CursoId == turma.CursoId);
+        }
     }
 }
